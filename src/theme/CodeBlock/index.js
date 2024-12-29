c08f2f8d90ddf65624d53f6b36badda580e81775
Grig <grig@lumeneo.com>
2024-12-29 10:22:52 -0700

    Add schema.org validator link to code blocks

---
+++ src/theme/CodeBlock/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import clsx from 'clsx';
+import CodeBlock from '@theme-original/CodeBlock';
+import styles from './styles.module.css';
+
+export default function CodeBlockWrapper(props) {
+    const validateCode = () => {
+        const encodedCode = encodeURIComponent(props.children);
+        // Try with additional parameters that might help
+        const validatorUrl = `https://validator.schema.org/?code=${encodedCode}`;
+        window.open(validatorUrl, '_blank');
+    };
+
+    return (
+        <div className={styles.codeBlockContainer}>
+            <CodeBlock {...props} />
+            <button
+                className={clsx(
+                    'clean-btn',
+                    styles.validateButton
+                )}
+                onClick={validateCode}
+            >
+                Validate
+            </button>
+        </div>
+    );
+}
